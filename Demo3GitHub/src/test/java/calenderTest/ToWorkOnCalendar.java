979331807cb4e8f2b6610d7acc7549171940e9a5
Admin <Admin@supriyavijay>
2024-08-13 10:31:10 +0530

    Added leads and Calendar module

---
+++ Demo3GitHub/src/test/java/calenderTest/ToWorkOnCalendar.java
@@ -0,0 +1,10 @@
+package calenderTest;
+
+public class ToWorkOnCalendar {
+
+	public static void main(String[] args) {
+
+		System.out.println("Work Complete");
+	}
+
+}
--- Demo3GitHub/src/test/java/organizationTest/ToCreateOrg.java
+++ Demo3GitHub/src/test/java/organizationTest/ToCreateOrg.java
@@ -5,6 +5,7 @@ public class ToCreateOrg {
 	public static void main(String[] args) {
 
 		System.out.println("Organization Create Successfully");
+		System.out.println("Added new Org");
 	}
 
 }
